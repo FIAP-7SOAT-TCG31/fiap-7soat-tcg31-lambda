e1cbe02fec742c9b0bd87ad55a19b16168d555d8
gm50x <getuliomagela@outlook.com>
2024-09-07 17:15:48 -0300

    fix: deploys image version instead of latest

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "image_version" {
+  description = "Container Image Version To Deploy"
+  default = ""
+}
+
 locals {
   aws_account_id = "287574492696"
   image_url      = "dkr.ecr.us-east-1.amazonaws.com/fiap_burger_lambda"
@@ -9,7 +14,7 @@ locals {
 
 resource "aws_lambda_function" "hello_world" {
   function_name = "hello-world"
-  image_uri     = "${local.aws_account_id}.${local.image_url}:latest"
+  image_uri     = "${local.aws_account_id}.${local.image_url}:${var.image_version}"
   role          = "arn:aws:iam::${local.aws_account_id}:role/LabRole"
   package_type  = "Image"
 }

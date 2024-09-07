6f7151d04529696e7db0df62056ad1bbc2832eaa
gm50x <getuliomagela@outlook.com>
2024-09-07 17:00:42 -0300

    fix: image registry name

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ provider "aws" {
 
 locals {
   aws_account_id = "287574492696"
-  ecr_url        = "dkr.ecr.us-east-1.amazonaws.com/fiap_burger_lambda"
+  image_url        = "dkr.ecr.us-east-1.amazonaws.com/fiap_burger_lambda"
 }
 
 resource "aws_lambda_function" "hello_world" {
   function_name = "hello-world"
-  image_uri     = "${local.aws_account_id}.${local.ecr_url}/hello-world:latest"
+  image_uri     = "${local.aws_account_id}.${local.image_url}:latest"
   role          = "arn:aws:iam::${local.aws_account_id}:role/LabRole"
   package_type  = "Image"
 }

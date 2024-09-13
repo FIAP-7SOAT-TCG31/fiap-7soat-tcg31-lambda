fc9e96af6e74552d9c36f1bb7b0506a5ec7801d4
gm50x <getuliomagela@outlook.com>
2024-09-13 19:14:58 -0300

    feat: adds new variables to lambda deployment config

--- main.tf
+++ main.tf
@@ -6,21 +6,20 @@ provider "aws" {
 # FIAP BURGER AUTH LAMBDA FUNCTION #
 ####################################
 
-locals {
-  aws_account_id = "287574492696"
-  image_url      = "dkr.ecr.us-east-1.amazonaws.com/fiap_burger_auth_lambda"
-}
-
 resource "aws_lambda_function" "fiap_burger_auth_lambda" {
   function_name = "fiap-burger-auth-lambda"
-  image_uri     = "${local.aws_account_id}.${local.image_url}:${var.image_version}"
-  role          = "arn:aws:iam::${local.aws_account_id}:role/LabRole"
+  image_uri     = "${var.aws_account_id}.dkr.ecr.us-east-1.amazonaws.com/fiap_burger_auth_lambda:${var.image_version}"
+  role          = "arn:aws:iam::${var.aws_account_id}:role/LabRole"
   package_type  = "Image"
 
   timeout = 180
   environment {
     variables = {
-      DUMMY = "DUMMY"
+      COGNITO_USER_POOL_ID   = var.cognito_user_pool_id
+      COGNITO_CLIENT_ID      = var.cognito_client_id
+      XAWS_ACCESS_KEY_ID     = var.aws_access_key_id
+      XAWS_SECRET_ACCESS_KEY = var.aws_secret_access_key
+      XAWS_SESSION_TOKEN     = var.aws_session_token
     }
   }
 }

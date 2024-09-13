2d70abb158d0650dade3648722b8f6f5a9994837
gm50x <getuliomagela@outlook.com>
2024-09-12 21:11:30 -0300

    fix: incorrect setup

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ resource "aws_api_gateway_method" "proxy_root" {
 resource "aws_api_gateway_integration" "lambda_root" {
   rest_api_id = aws_api_gateway_rest_api.fiap_burger_identity.id
   resource_id = aws_api_gateway_method.proxy_root.resource_id
-  http_method = aws_api_gateway_method.proxy_root.method
+  http_method = aws_api_gateway_method.proxy_root.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
